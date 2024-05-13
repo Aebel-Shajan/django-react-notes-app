ad90d36943d6051db4906339fb59b058e7ec8f00
Aebel Shajan <67755450+Aebel-Shajan@users.noreply.github.com>
2024-05-13 09:59:40 +0100

    Add logic to create, delete and get notes on the home page

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,9 +1,84 @@
+import { useEffect, useState } from "react";
+import api from "../api";
+
 const Home = () => {
-  return ( 
+  const [notes, setNotes] = useState([]);
+  const [content, setContent] = useState("");
+  const [title, setTitle] = useState("");
+
+  useEffect(() => {
+    getNotes();
+  }, [])
+
+  const getNotes = () => {
+    api
+      .get("/api/notes/")
+      .then(res => res.data)
+      .then((data) => { setNotes(data); console.log(data); })
+      .catch(err => alert(err))
+  }
+
+  const deleteNote = (id) => {
+    api
+      .delete(`/api/notes/delete/${id}`)
+      .then(res => {
+        if (res.status === 204) {
+          alert("Note deleted!")
+        } else {
+          alert("Failed to delete note")
+        }
+        getNotes();
+      })
+      .catch(err => alert(err))
+  }
+
+  const createNote = (e) => {
+    e.preventDefault();
+    api
+      .post("/api/notes/", { content, title })
+      .then(res => {
+        if (res.status === 201) {
+          alert("Note created!")
+        } else {
+          alert("Failed to create note!")
+        }
+        getNotes();
+      })
+      .catch(err => alert(err))
+  }
+
+  return (
     <div>
-    Home
+      <div>
+        <h2>Notes</h2>
+
+      </div>
+      <form onSubmit={createNote}>
+        <label htmlFor="title">Title: </label>
+        <br />
+        <input
+          type="text"
+          id="title"
+          name="title"
+          required
+          onChange={e => setTitle(e.target.value)}
+          value={title}
+        />
+        <label htmlFor="content">Content: </label>
+        <br />
+        <textarea
+          id="content"
+          name="content"
+          required
+          onChange={e => setContent(e.target.value)}
+          value={content}
+        >
+        </textarea>
+        <br />
+        <input type="submit" value="Submit" />
+      </form>
     </div>
-   );
+  );
 }
- 
+
 export default Home;
