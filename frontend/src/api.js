33d68b75f80b1bf50497fe0f1e214b53e19e7e99
Aebel Shajan <67755450+Aebel-Shajan@users.noreply.github.com>
2024-05-13 09:59:19 +0100

    Fix incorrect naming of baseURL

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -5,20 +5,20 @@ import axios from "axios";
 import { ACCESS_TOKEN } from "./constants";
 
 const api = axios.create({
-  baseUrl : import.meta.env.VITE_API_URL
-})
+  baseURL : import.meta.env.VITE_API_URL
+});
 
 api.interceptors.request.use(
   (config) => {
     const token = localStorage.getItem(ACCESS_TOKEN);
     if (token) {
-      config.headers.Authorization = `Bearer ${token}`
+      config.headers.Authorization = `Bearer ${token}`;
     }
-    return config
+    return config;
   },
   (error) => {
-    return Promise.reject(error)
+    return Promise.reject(error);
   }
-)
+);
 
-export default api
+export default api;
