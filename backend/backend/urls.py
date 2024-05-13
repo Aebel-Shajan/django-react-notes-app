90c51f67ddaa594eadfe36a72c9d5c9a01b63667
Aebel Shajan <67755450+Aebel-Shajan@users.noreply.github.com>
2024-05-13 09:57:21 +0100

    Write create, read & delete views. 50:00

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path("api/user/register/", CreateUserView.as_view(), name="register"),
     path("api/token/", TokenObtainPairView.as_view(), name="get_token"),
     path("api/token/refresh", TokenRefreshView.as_view(), name="refresh"),
-    path("api-auth/", include("rest_framework.urls"))
+    path("api-auth/", include("rest_framework.urls")),
+    path("api/", include("api.urls"))
 ]
