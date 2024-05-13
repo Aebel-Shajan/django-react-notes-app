a80f16d64b81400172ffa753b778d4c538e0b63f
Aebel Shajan <67755450+Aebel-Shajan@users.noreply.github.com>
2024-05-13 09:57:29 +0100

    Setup Frontend boilerplate. Setup axios api object

---
+++ frontend/src/api.js
@@ -0,0 +1,24 @@
+// Interceptor will intercept any request we are gonna send and automatically 
+// add the correct header so we dont need to manually write it every time
+
+import axios from axios;
+import { ACCESS_TOKEN } from "./constants";
+
+const api = axios.create({
+  baseUrl : import.meta.env.VITE_API_URL
+})
+
+api.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem(ACCESS_TOKEN);
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`
+    }
+    return config
+  },
+  (error) => {
+    return Promise.reject(error)
+  }
+)
+
+export default api
