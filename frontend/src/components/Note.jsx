479970646dca8d340d4c7eaa3fb000b40163db37
Aebel Shajan <67755450+Aebel-Shajan@users.noreply.github.com>
2024-05-13 09:59:55 +0100

    Create note component. 1:46

---
+++ frontend/src/components/Note.jsx
@@ -0,0 +1,17 @@
+
+
+const Note = ({note, onDelete}) => {
+  const formatedDate = new Date(note.created_at).toLocaleDateString("en-US");
+  return ( 
+    <div className="note-container">
+      <p className="note-title">{note.title}</p>
+      <p className="note-content">{note.content}</p>
+      <p className="note-date">{}</p>
+      <button className="delete-button" onClick={()=> onDelete(note.id)}>
+        Delete
+        </button>
+    </div>
+   );
+}
+ 
+export default Note;
