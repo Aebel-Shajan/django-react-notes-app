089cca02206e8be66044c494624079d0563c1b0e
Aebel Shajan <67755450+Aebel-Shajan@users.noreply.github.com>
2024-05-13 09:58:20 +0100

    Add missing logout path

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -28,24 +28,10 @@ function App() {
             </ProtectedRoute>
           }
         />
-        <Route
-          path="/login"
-          element={
-            <Login />
-          }
-        />
-        <Route
-          path="/register"
-          element={
-            <RegisterAndLogout />
-          }
-        />
-        <Route
-          path="*"
-          element={
-            <NotFound />
-          }
-        />
+        <Route path="/login" element={<Login />}/>
+        <Route path="/logout" element={<Logout />}/>
+        <Route path="/register" element={<RegisterAndLogout />}/>
+        <Route path="*" element={<NotFound />}/>
       </Routes>
     
     </BrowserRouter>
