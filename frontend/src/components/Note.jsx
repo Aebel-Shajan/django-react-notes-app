1fc1c5e854c517f0257a8b52b12333ce89d5def8
Aebel Shajan <67755450+Aebel-Shajan@users.noreply.github.com>
2024-05-13 10:00:36 +0100

    Add missing date from note component

--- frontend/src/components/Note.jsx
+++ frontend/src/components/Note.jsx
@@ -6,7 +6,7 @@ const Note = ({note, onDelete}) => {
     <div className="note-container">
       <p className="note-title">{note.title}</p>
       <p className="note-content">{note.content}</p>
-      <p className="note-date">{}</p>
+      <p className="note-date">{formatedDate}</p>
       <button className="delete-button" onClick={()=> onDelete(note.id)}>
         Delete
         </button>
